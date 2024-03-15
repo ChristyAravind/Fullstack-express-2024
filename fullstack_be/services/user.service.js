c028018d0a8849dd28365ef34a4aa31de6cf4f7b
ChristyAravind <aravindmanimozhi@gmail.com>
2024-03-15 15:34:35 +0530

    login api changes

--- fullstack_be/services/user.service.js
+++ fullstack_be/services/user.service.js
@@ -1,26 +1,36 @@
 const User = require("../schemas/user.schema");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //Create or Register User
 exports.createUser = async (req, res) => {
-    try {
-      const getUser = await User.findOne
-      ({
-       email: req.body.email
+  try {
+    const getUser = await User.findOne({
+      email: req.body.email,
+    });
+    if (getUser) {
+      return res.status(200).json({
+        statusCode: 410,
+        message: "User Exist. Please login",
+        success: false,
       });
-      if(getUser) {
-        return res.status(400).json
-        ({
-          statusCode: 200, 
-          message:"User Exist. Please login",
-          success: false, 
-        });
-      }
+    }
     req.body.password = await bcrypt.hash(req.body.password, +process.env.SALT);
-    
-    const user = await User.create( req.body);
-    res?.status(200)?.json({ success: true });
+    req.body.role = "user";
+
+    const user = await User.create(req.body);
+    const token = jwt.sign(
+      { email: user.email, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    res?.status(200)?.json({
+      statusCode: 200,
+      message: "User Register Successfully",
+      success: true,
+      data: { user, token },
+    });
   } catch (error) {
     res.status(400).json({ success: false, err: error.message });
   }
@@ -30,13 +40,11 @@ exports.createUser = async (req, res) => {
 exports.getUser = async (req, res) => {
   try {
     const getUser = await User.find();
-    res?.status(200)?.json(
-        { 
-            statusCode: 200,
-            data:getUser,
-            success: true
-        })
-        ;
+    res?.status(200)?.json({
+      statusCode: 200,
+      data: getUser,
+      success: true,
+    });
   } catch (error) {
     res.status(400).json({ success: false, err: err });
   }
@@ -45,16 +53,12 @@ exports.getUser = async (req, res) => {
 //Get User by ID
 exports.getUserId = async (req, res) => {
   try {
-    const getUser = await User.findById(
-        req.params.id
-    );
-    res?.status(200)?.json(
-        { 
-            statusCode: 200,
-            data:getUser,
-            success: true
-        })
-        ;
+    const getUser = await User.findById(req.params.id);
+    res?.status(200)?.json({
+      statusCode: 200,
+      data: getUser,
+      success: true,
+    });
   } catch (error) {
     res.status(400).json({ success: false, err: err });
   }
@@ -62,18 +66,18 @@ exports.getUserId = async (req, res) => {
 
 //Update User by ID
 exports.updateUser = async (req, res) => {
-    console.log('req.body::: ', req.body);
-    console.log('req.params.id::: ', req.params.id);
+  console.log("req.body::: ", req.body);
+  console.log("req.params.id::: ", req.params.id);
   try {
-    const getUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const getUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
 
-    res?.status(200)?.json(
-        { 
-            statusCode: 200,
-            data:getUser,
-            success: true
-        })
-        ;
+    res?.status(200)?.json({
+      statusCode: 200,
+      data: getUser,
+      success: true,
+    });
   } catch (error) {
     res.status(400).json({ success: false, err: err });
   }
@@ -82,24 +86,31 @@ exports.updateUser = async (req, res) => {
 //Login
 exports.userLogin = async (req, res) => {
   try {
-    const getUser = await User.findOne
-    ({
-     email: req.body.email
+    const getUser = await User.findOne({
+      email: req.body.email,
     });
-    if(!getUser) return res.status(400).json({ message:"Please Register",success: false});
-   let response = await bcrypt.compare(req?.body?.password, getUser.password)
-   if(!response) return res.status(400).json({ message:"Invalid Credentials",success: false});
+    if (!getUser)
+      return res
+        .status(400)
+        .json({ message: "Please Register", success: false });
+    let response = await bcrypt.compare(req?.body?.password, getUser.password);
+    if (!response)
+      return res
+        .status(400)
+        .json({ message: "Invalid Credentials", success: false });
 
-   const token = jwt.sign({  email: getUser.email, role: getUser.role }, process.env.JWT_SECRET ,{expiresIn:process.env.JWT_EXPIRES_IN});
+    const token = jwt.sign(
+      { email: getUser.email, role: getUser.role },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
 
-    res?.status(200)?.json(
-        { 
-            statusCode: 200,
-            data:getUser,
-            success: true,
-            token
-        })
-        ;
+    res?.status(200)?.json({
+      statusCode: 200,
+      data: { getUser, token },
+      success: true,
+      message: "Login successful",
+    });
   } catch (error) {
     res.status(400).json({ success: false, err: error });
   }
